e2746c50b50c930a29501cfac72896c49c54d023
xfiph <47494108+xfiph@users.noreply.github.com>
2022-10-10 07:00:39 +0700

    Add files via upload

---
+++ additional-material/winsock_reset.bat
@@ -0,0 +1,7 @@
+@echo off   
+title winsock reset tool (win2003,xp,Win7,Win8,Win10)
+echo notice£ºPlease run with the administrator permissions
+echo network reseting£¬ wait..... 
+netsh winsock reset
+color 0a 
+echo. & pause    
